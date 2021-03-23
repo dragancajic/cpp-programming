7467856530d082addc6cb82016202cb8705dfde8
Dragan Cajic <cajic_dragan@yahoo.com>
2021-03-23 15:24:03 +0100

    bool isSimpleNumber(int) for positive integers

--- chapter1.cpp
+++ chapter1.cpp
@@ -30,6 +30,20 @@ double prosjekOcjena(int ocjena1, int ocjena2, int ocjena3, int ocjena4) {
 // Napisati funkciju koja racuna zapreminu V valjka.
 // P = r^2 * PI * H
 
+// ...
+
+// primjeri funkcija
+bool daLiJeProstBroj(int broj) {
+	//if (broj % 2 == 0) // djeljiv sa 2 ako je TRUE!
+
+	for (int i = 2; i < broj; i++) {
+		if (broj % i == 0) { // AKO je djeljiv sa 2, 3, 4, 5,...
+			return false;    // do broj-1, ONDA NIJE prost broj!
+		}
+	}
+	// u protivnom JESTE prost broj, pa funcija vraca TRUE!
+	return true;
+}
 
 int main() {
 	// interaktivnost
@@ -52,6 +66,22 @@ int main() {
 	cout << "zakljucna ocjena: " << zakljucna << endl;
 	cout << "zapremine kvadra su: " << zapremina1 << ", " << zapremina2 << ", "
 		<< zapremina3a << ", " << zapremina3b << endl;
+
+	// testiranje i pozivanje funkcije
+	// povratni-tip imeFunkcije(tip-parametra nazivParametra, ...) {
+	// 		tijelo-funkcije;
+	//		instrukcije/komande;
+	// }
+	// bool daLiJeProstBroj(int) //--> potpis funkcije
+	int broj = 0; // deklaracija promjenljive za cuvanje vrijednosti broja
+	cout << "Unesite pozitivan broj: "; // ispis na konzolu
+	cin >> broj; // korisnik na konzoli unosi broj
+
+	// ako je povratna vrijednost funkcije TRUE, broj je prost
+	if (daLiJeProstBroj(broj))
+		cout << "Broj " << broj << " JESTE prost." << endl;
+	else // ako funkcija vraca FALSE, nije prost!
+		cout << "Broj " << broj << " NIJE prost!" << endl;
 
 	return 0;
 }
